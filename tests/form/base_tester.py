79a6795d4ea90d8d2115729caeaa7ea8a68a3be7
vainga <64656614+vainga@users.noreply.github.com>
2025-03-02 22:27:36 +0700

    final2

--- tests/form/base_tester.py
+++ tests/form/base_tester.py
@@ -1,59 +1,34 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from typing import Type, Optional
+from typing import Type, Optional, Union
 
 from django.db.models import Model
 from django.test import Client
 
+from conftest import TitledUrlRepr
 from fixtures.types import ModelAdapterT
 
 
 class BaseTester(ABC):
-
-    @property
     @abstractmethod
-    def of_which_action(self):
-        ...
-
-    @property
-    @abstractmethod
-    def which_obj(self):
-        ...
-
-    @property
-    @abstractmethod
-    def of_which_obj(self):
-        ...
-
-    @property
-    @abstractmethod
-    def one_and_only_one(self):
-        ...
-
-    @property
-    def on_which_page(self):
-        return f'на странице {self.of_which_action} {self.of_which_obj}'
-
-    @property
-    def to_which_page(self):
-        return f'на страницу {self.of_which_action} {self.of_which_obj}'
+    def redirect_error_message(
+        self, by_user: str, redirect_to_page: Union[TitledUrlRepr, str]
+    ):
+        raise NotImplementedError
 
     @abstractmethod
-    def redirect_error_message(self, by_user: str, redirect_to_page: str):
-        ...
-
-    @abstractmethod
-    def status_error_message(self, by_user: str):
-        ...
+    def status_error_message(self, by_user: str) -> str:
+        raise NotImplementedError
 
     def __init__(
-            self,
-            model_cls: Type[Model],
-            user_client: Client,
-            another_user_client: Optional[Client] = None,
-            unlogged_client: Optional[Client] = None,
-            item_adapter: ModelAdapterT = None):
+        self,
+        model_cls: Type[Model],
+        user_client: Client,
+        another_user_client: Optional[Client] = None,
+        unlogged_client: Optional[Client] = None,
+        item_adapter: ModelAdapterT = None,
+    ):
         self.user_client = user_client
         self.another_user_client = another_user_client
         self.unlogged_client = unlogged_client
